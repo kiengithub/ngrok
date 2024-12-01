b29d37489f81d025f43ac1a7759b490f8c97dab4
kiengithub <94677260+kiengithub@users.noreply.github.com>
2024-12-01 14:49:15 +0700

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,6 @@ if [ -z ${FORWARD_HOST+x} ] || [ -z ${FORWARD_PORT+x} ]; then
    echo "FORWARD_HOST and FORWARD_PORT are required environment variables"
    exit 1
 fi
-ngrok authtoken 2pbEcRiSlsfNdALNW1GMLcrHSHU_5Q9xKWE2An8pCwBxkDPxN &
+ngrok authtoken 2pbed5tPEdmrjcmouHXqlrtThnY_62qbsnyk6sHgyphPtyvRu &
 ngrok http "${FORWARD_HOST}:${FORWARD_PORT}" &
 exec socat TCP-LISTEN:8080,fork,reuseaddr TCP:localhost:4040 
